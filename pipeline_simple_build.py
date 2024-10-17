bd88e1f0f01c73a27442c30c286d736ccf86bb67
Anderson Viana <andviana@hotmail.com>
2024-10-16 23:18:37 -0300

    updated pipelines

--- pipeline_simple_build.py
+++ pipeline_simple_build.py
@@ -2,19 +2,23 @@ import luigi
 import toml
 import time
 import os
+import shutil
 
 
 class BaixarDados(luigi.Task):
+    
     def output(self):
         return luigi.LocalTarget(config['local_files']['input_file'])
 
     def run(self):
+        time.sleep(30)
+        
         with self.output().open('w') as f:
             f.write('dados de exemplo')
-        time.sleep(60)  # Pausa de 1 minuto
-
-
+    
+    
 class LimparDados(luigi.Task):
+    
     def requires(self):
         return BaixarDados()
 
@@ -22,14 +26,19 @@ class LimparDados(luigi.Task):
         return luigi.LocalTarget(config['local_files']['cleaned_file'])
 
     def run(self):
+        time.sleep(30)
+
         with self.input().open('r') as input_file:
             dados = input_file.read()
+            
         dados_limpos = dados.replace('exemplo', 'limpo')
+        
         with self.output().open('w') as output_file:
             output_file.write(dados_limpos)
-        time.sleep(60)  # Pausa de 1 minuto
-
+        
+        
 class ProcessarDados(luigi.Task):
+    
     def requires(self):
         return LimparDados()
 
@@ -37,14 +46,19 @@ class ProcessarDados(luigi.Task):
         return luigi.LocalTarget(config['local_files']['processed_file'])
 
     def run(self):
+        time.sleep(30)  
+        
         with self.input().open('r') as input_file:
             dados = input_file.read()
+            
         dados_processados = dados.upper()
+        
         with self.output().open('w') as output_file:
             output_file.write(dados_processados)
-        time.sleep(60)  # Pausa de 1 minuto
+        
 
 class GerarRelatorio(luigi.Task):
+    
     def requires(self):
         return ProcessarDados()
 
@@ -52,28 +66,31 @@ class GerarRelatorio(luigi.Task):
         return luigi.LocalTarget(config['local_files']['report_file'])
 
     def run(self):
+        time.sleep(30)
+        
         with self.input().open('r') as input_file:
             dados = input_file.read()
+            
         relatorio = f"Relatório de Dados:\n\n{dados}"
+        
         with self.output().open('w') as output_file:
             output_file.write(relatorio)
-        time.sleep(60)  # Pausa de 1 minuto
+        
 
 class Pipeline(luigi.WrapperTask):
+    
     def requires(self):
         return GerarRelatorio()
 
+
 if __name__ == '__main__':
-    
-    
-    
     # Carrega a configuração
     config = toml.load('config.toml')
+    shutil.rmtree('dados')
     os.makedirs('dados', exist_ok = True)
     # luigi.run()
+    # sem o wrapper (executa diretamente o GerarRelatorio)
+    luigi.build([GerarRelatorio()], scheduler_host = "localhost")
 
-    # sem o wrapper
-    luigi.build([GerarRelatorio()], workers=5, scheduler_host = "localhost")
-
-    #to execute run this command on terminal
-    # .venv/bin/python3 my_pipeline.py Pipeline --scheduler-host localhost
+    # run this command on terminal
+    # .venv/bin/python3 pipeline_simple_build.py Pipeline --scheduler-host localhost
--- pipeline_simple_run.py
+++ pipeline_simple_run.py
@@ -2,54 +2,67 @@ import luigi
 import toml
 import time
 import os
+import shutil
+
 
 class Tarefa1(luigi.Task):
+    
     def requires(self):
         return []
 
     def output(self):
-        return luigi.LocalTarget(config['local_files']['numbers_up_to_10_file'])
+        return luigi.LocalTarget(config['pipe_simple']['numbers_up_to_10_file'])
 
     def run(self):
+        time.sleep(30)
+
         with self.output().open('w') as f:
             for i in range(1, 11):
                 f.write("{}\n".format(i))
 
-        time.sleep(30)
+       
 
 class Tarefa2(luigi.Task):
+    
     def requires(self):
         return Tarefa1()
 
     def output(self):
-        return luigi.LocalTarget(config['local_files']['squares_file'])
+        return luigi.LocalTarget(config['pipe_simple']['squares_file'])
 
     def run(self):
+        time.sleep(30)
+        
         with self.input().open() as fin, self.output().open('w') as fout:
             for line in fin:
                 n = int(line.strip())
                 out = n * n
                 fout.write("{}:{}\n".format(n, out))
-        time.sleep(30)
+        
+
 
 class Tarefa3(luigi.Task):
+    
     def requires(self):
         return Tarefa2()
 
     def output(self):
-        return luigi.LocalTarget(config['local_files']['cubes_file'])
+        return luigi.LocalTarget(config['pipe_simple']['cubes_file'])
         
     def run(self):
+        time.sleep(30)
+        
         with self.input().open() as fin, self.output().open('w') as fout:
             for line in fin:
                 n, _ = line.strip().split(":")
                 out = int(n) ** 3
                 fout.write("{}:{}\n".format(n, out))
-        time.sleep(30)
+        
 
 if __name__ == '__main__':
     config = toml.load('config.toml')
+    shutil.rmtree('dados')
     os.makedirs('dados', exist_ok = True)
     luigi.run()
 
-#.venv/bin/python3 -m pipeline2 Tarefa3 --workers 2 --scheduler-host localhost --scheduler-port 8082
+#.venv/bin/python3 -m pipeline_simple_run Tarefa3 --scheduler-host localhost --scheduler-port 8082
